449d708f03f75c2946eed5ef9dcf806afa10b1d0
ansnoussi <anis0snoussi@gmail.com>
2019-01-01 17:56:36 +0000

    file_command

--- Forensics_Warmup_2/extract_magic.sh
+++ Forensics_Warmup_2/extract_magic.sh
@@ -3,3 +3,7 @@
 hexdump flag.jpg | head -1
 
 #where to look next: https://en.wikipedia.org/wiki/List_of_file_signatures
+
+
+#you can also check the file type using the tool: file
+# Usage: file flag.jpg
